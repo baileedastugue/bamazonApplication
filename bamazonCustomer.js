43f291887f83152ce6dc8a47b1a17c092f899cdc
Bailee Dastugue <bailee.dast@gmail.com>
2020-04-17 11:43:45 -0500

    display products returns what's in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,11 +14,12 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("hey");
-    // addProducts();
+    // addStockProducts();
+    displayProducts();
 })
 
 var insert = "insert into products (product_name, department_name, price, stock_quantity, product_sales) values ?";
-var products = [
+var stockProducts = [
     ["apple", "grocery", 1.4, 15],
     ["barbells", "fitness", 25, 8],
     ["sticky notes", "home", 5, 10],
@@ -27,20 +28,35 @@ var products = [
     ["coffee", "grocert", 11.99, 30],
     ["toothpaste", "pharmacy", 6.35, 30]
 ]
-for (var i = 0; i < products.length; i++) 
+for (var i = 0; i < stockProducts.length; i++) 
     {
-        products[i].push(products[i][2]*products[i][3]);
+        stockProducts[i].push(stockProducts[i][2]*stockProducts[i][3]);
     }
-console.log(products);
 
 
-// function addProducts() {
-//     for (var i = 0; i < products.length; i++) {
-//     var query = connection.query(
-//         insert, [], 
-//          function (err, res) {
-//             if (err) throw err;
-//             console.log(res.affectedRows + " product inserted!\n");
-//         })
-//     }
-// }
+function addStockProducts() {
+    var query = connection.query(
+        insert, [stockProducts], 
+         function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " row inserted!\n");
+        })
+}
+
+function displayProducts () {
+    var display = "select * from products";
+    var query = connection.query(
+        display, function (err, res) {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+            console.log("Product ID: " + res[i].item_id + 
+                "\nProduct Name: " + res[i].product_name + 
+                "\nDepartment Name: " + res[i].department_name + 
+                "\nPrice: " + res[i].price + 
+                "\nQuantity in Stock: " + res[i].stock_quantity + 
+                "\nProduct Sale: " + res[i].product_sales +
+                "\n=========================");
+            }
+        }
+    )
+}
